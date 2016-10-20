55386d117921388957474632fea8da3c4af81cb5
Henry Dotson <l3iodeez@gmail.com>
2016-10-20 08:25:18 -0400

    Fix doc ready.

--- navbar.js
+++ navbar.js
@@ -1,8 +1,9 @@
-(function (root) {
+(function(root) {
 var BensNavBar = root.BensNavBar = {};
 BensNavBar.touch = false;
+BensNavBar.mailingListLink = 'https://btn.ymlp.com/xgesummygmgm';
 
-BensNavBar.addClass = function (element, newClass) {
+BensNavBar.addClass = function(element, newClass) {
   console.log('added ' + newClass + ' to ' + element.id);
   classList = element.className.split(' ');
   for (var i = 0; i < classList.length; i++) {
@@ -14,7 +15,7 @@ BensNavBar.addClass = function (element, newClass) {
   element.className = classList + ' ' + newClass;
 };
 
-BensNavBar.removeClass = function (element, removalClass) {
+BensNavBar.removeClass = function(element, removalClass) {
   console.log('removed ' + removalClass + ' from ' + element.id);
   var classList = element.className.split(' ');
   for (var i = 0; i < classList.length; i++) {
@@ -26,7 +27,7 @@ BensNavBar.removeClass = function (element, removalClass) {
   element.className = classList.join(' ');
 };
 
-BensNavBar.hoverOn = function (element) {
+BensNavBar.hoverOn = function(element) {
   if (element !== BensNavBar.currentHoverElement) {
     if (typeof BensNavBar.currentHoverElement !== 'undefined') {
       BensNavBar.removeClass(BensNavBar.currentHoverElement, 'hoveron');
@@ -42,7 +43,7 @@ BensNavBar.hoverOn = function (element) {
   }
 };
 
-BensNavBar.handleTouchStart = function (evt) {
+BensNavBar.handleTouchStart = function(evt) {
   evt.stopPropagation();
   evt.preventDefault();
   BensNavBar.touch = true;
@@ -51,6 +52,8 @@ BensNavBar.handleTouchStart = function (evt) {
   if (['menubutton', 'menuimage'].includes(hoverElement.className)) {
     BensNavBar.addClass(menuButton, 'touch');
     BensNavBar.addClass(document.getElementById('drophover'), 'show');
+  } else if (['emaillist'].includes(hoverElement.className)) {
+    window.open(BensNavBar.mailingListLink, '_blank');
   }
 };
 
@@ -61,7 +64,7 @@ BensNavBar.handleTouchMove = function handleTouchMove(evt) {
   BensNavBar.hoverOn(hoverElement);
 };
 
-BensNavBar.handleTouchEnd = function (evt) {
+BensNavBar.handleTouchEnd = function(evt) {
       BensNavBar.touch = false;
       BensNavBar.removeClass(document.getElementById('menubutton'), 'touch');
       if (typeof BensNavBar.currentHoverElement !== 'undefined') {
@@ -79,9 +82,11 @@ BensNavBar.handleClick = function functionName(element) {
 document.addEventListener('touchstart', BensNavBar.handleTouchStart, false);
 document.addEventListener('touchmove', BensNavBar.handleTouchMove, false);
 document.addEventListener('touchend', BensNavBar.handleTouchEnd, false);
-document.getElementById('menubutton').addEventListener('click', function (evt) {
+window.onload = function() {
+  document.getElementById('menubutton').addEventListener('click', function(evt) {
     evt.stopPropagation();
     evt.preventDefault();
   }, false);
+};
 
 }(this));
